2d23aec4934baa1949079f53c2a03ef6e109943f
AndyPandy40 <130397916+AndyPandy40@users.noreply.github.com>
2024-11-19 09:49:43 +0000

    Started work on the game class

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
 
 pygame.init()
+
+class Game:
+    def __init__(self, screen_width, screen_height, money, score, bee_background_image, screen):
+        self.screen_width = 1440
+        self.screen_height = 864
+        self.money = 0
+        self.score = 0
+        self.bee_background_image = pygame.image.load("images/title_screen_bee.png")
