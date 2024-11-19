7399d29d1fca01afecd3378e5ac4f261a84ffb9c
AndyPandy40 <theandrewstott@gmail.com>
2024-11-19 21:28:44 +0000

    Continued work on the "Game" class

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import pygame
 pygame.init()
 
 class Game:
-    def __init__(self, screen_width, screen_height, money, score, bee_background_image, screen):
+    def __init__(self):
         self.screen_width = 1440
         self.screen_height = 864
         self.money = 0
         self.score = 0
+        self.background_color_image = 0 #add thissss
         self.bee_background_image = pygame.image.load("images/title_screen_bee.png")
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+
+    def display_menu():
+        pygame.blit
+
