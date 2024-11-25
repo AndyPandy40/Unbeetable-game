67a6be0840e7b50bb250457b2a2718fa7ed5d7bf
AndyPandy40 <130397916+AndyPandy40@users.noreply.github.com>
2024-11-25 10:49:21 +0000

    Got rectangles appearing for the buttons

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
-
 pygame.init()
+
+
+# Colour constants
+GREEN = (0, 255, 0)
+LIGHT_GREEN = (0, 200, 0)
+RED = (255, 0, 0)
+LIGHT_RED = (200, 0, 0)
 
 class Game:
     def __init__(self):
@@ -11,17 +17,42 @@ class Game:
         self.background_color_image = pygame.image.load("images/grass_background.png")
         self.bee_background_image = pygame.image.load("images/title_screen_bee.png")
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # Display the menu
+            NewGame.display_menu()
 
     def display_menu(self):
         self.screen.blit(self.background_color_image, (0,0))
         self.screen.blit(self.bee_background_image, (0,0))
-        StartButton = Button(0, 0, )
+
+        # Create and display start button
+        StartButton = Button(GREEN, LIGHT_GREEN, "Start", (216, 550), 450, 100, self.start_game)
+        StartButton.draw_button(self.screen)
+
+        # Create and display quit button
+        QuitButton = Button(RED, LIGHT_RED, "Quit", (774, 550), 450, 100, self.quit)
+        QuitButton.draw_button(self.screen)
+
+    
         pygame.display.update()
         
+    def start_game(self):
+        pass
+
+    def quit(self):
+        self.running = False
+        pygame.quit()
 
 
 class Button:
-    def __init__(self, inactive_color, active_color, text, position, width, height, action):
+    def __init__(self, inactive_color, active_color, text, position, width, height, action=None):
         self.inactive_color = inactive_color
         self.active_color = active_color
         self.text = text
@@ -31,22 +62,19 @@ class Button:
         self.action = action
 
     def draw_button(self, screen):
-        pygame.draw.rect(screen, self.inactive_color, ((self.position), self.width, self.height))
+        pygame.draw.rect(screen, self.inactive_color, (self.position[0], self.position[1], self.width, self.height))
+
 
     def is_hovered(self):
-        pass
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
 
 NewGame = Game()
 
-# Main game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # Display the menu
-    NewGame.display_menu()
+
+NewGame.run()
+
     
 pygame.quit()
 
