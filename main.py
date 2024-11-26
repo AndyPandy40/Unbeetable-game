55760334e7e81f806a25d26318bbccb88c960713
AndyPandy40 <130397916+AndyPandy40@users.noreply.github.com>
2024-11-26 09:51:13 +0000

    fixed fps

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ GREEN = (0, 255, 0)
 LIGHT_GREEN = (0, 200, 0)
 RED = (255, 0, 0)
 LIGHT_RED = (200, 0, 0)
-BLACK = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 class Game:
     def __init__(self):
@@ -15,10 +15,20 @@ class Game:
         self.screen_height = 864
         self.money = 0
         self.score = 0
+        self.frame = 0
         self.background_color_image = pygame.image.load("images/grass_background.png")
         self.bee_background_image = pygame.image.load("images/title_screen_bee.png")
+
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.running = True
+        self.clock = pygame.time.Clock()
+
+
+        self.start_button = Button(GREEN, LIGHT_GREEN, "Start", (216, 550), 450, 100, self.start_game)
+        self.quit_button = Button(RED, LIGHT_RED, "Quit", (774, 550), 450, 100, self.quit)
+
+        self.screen.blit(self.background_color_image, (0,0))
+        self.screen.blit(self.bee_background_image, (0,0))
 
     def run(self):
         while self.running:
@@ -28,19 +38,21 @@ class Game:
 
             # Display the menu
             NewGame.display_menu()
+            self.clock.tick(200)
+            print(self.clock.get_fps())
 
     def display_menu(self):
-        self.screen.blit(self.background_color_image, (0,0))
-        self.screen.blit(self.bee_background_image, (0,0))
+        
+
+
 
         # Create and display start button
-        StartButton = Button(GREEN, LIGHT_GREEN, "Start", (216, 550), 450, 100, self.start_game)
-        StartButton.draw_button(self.screen)
+        self.start_button.draw_button(self.screen)
 
         # Create and display quit button
-        QuitButton = Button(RED, LIGHT_RED, "Quit", (774, 550), 450, 100, self.quit)
-        QuitButton.draw_button(self.screen)
+        self.quit_button.draw_button(self.screen)
 
+        self.frame += 1
     
         pygame.display.update()
         
@@ -67,20 +79,23 @@ class Button:
         click = pygame.mouse.get_pressed()
 
 
-        # Display text on button
-        font = pygame.font.Font("freesansbold.ttf", 20)
-        text = font.render(self.text, True, BLACK)
-
-        button_centre = self.position[0] + (self.width/2)
-        text_rect = text.get_rect(centre=(button_centre))
-        
-        screen.blit(text, text_rect)
 
         # check if user is hovering over button
         if self.position[0] < mouse[0] < self.position[0] + self.width and self.position[1] < mouse[1] < self.position[1] + self.height:
-            pygame.draw.rect(screen, self.active_color, (self.position[0], self.position[1], self.width, self.height))
+            button_color = self.active_color
         else:
-            pygame.draw.rect(screen, self.inactive_color, (self.position[0], self.position[1], self.width, self.height))
+            button_color = self.inactive_color
+
+        pygame.draw.rect(screen, button_color, (self.position[0], self.position[1], self.width, self.height))
+        
+        # Display text on button
+        font = pygame.font.Font("freesansbold.ttf", 60)
+        text = font.render(self.text, True, BLACK)
+
+        button_center = ((self.position[0] + (self.width//2)), self.position[1] + (self.height//2))
+        text_rect = text.get_rect(center=(button_center))
+        
+        screen.blit(text, text_rect)
 
 
 
