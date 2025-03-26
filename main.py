95310689318cadb89e2d1d7f71ac5f4102da6e95
AndyPandy40 <theandrewstott@gmail.com>
2025-03-26 19:38:43 +0000

    Changed bee spawn rate

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ class MainGame:
         # Set up spawning bees
         self.bee_array = []
         self.last_bee_spawned = 0
-        self.bee_spawn_cooldown = 2500
+        self.bee_spawn_cooldown = 4000
         self.bee_health = 250
         self.number_of_bees_spawned = 0
 
@@ -233,7 +233,7 @@ class MainGame:
 
 
             # Slowly decrease bee spawn time
-            self.bee_spawn_cooldown -= self.current_time/5000000
+            self.bee_spawn_cooldown -= self.current_time/1000000
 
             if self.bee_spawn_cooldown <= 400:
                 self.bee_spawn_cooldown = 400
